609a27bf72fc06fa7f28728e6e90666a99c75905
JohanOrtega28 <125934568+JohanOrtega28@users.noreply.github.com>
2023-03-06 18:59:34 -0500

    Taller4

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,48 +1,54 @@
 package classroom;
 
 public class Persona {
+	
+	long cedula=1;
+	String nombre;
+	static int totalPersonas;
+	
+	static {
+		totalPersonas = 0;
+	}
+	
+	Persona() {
+		cedula = 0;
+		totalPersonas++;
+	}
+	
+	Persona(long cedula,String nombre) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		totalPersonas++;
+	}
+	
+	Persona(String nombre, long cedula) {
+		this.cedula = cedula;
+		this.nombre = "";
+		totalPersonas++;
+	}
+	
+	Persona(long cedula) {
+		this.cedula = cedula;
+		this.nombre = "";
+		totalPersonas++;
+	}
+	
+	Persona(String nombre) {
+		this.nombre = "";
+		totalPersonas++;
+	
+	}
+	
+	long getCedula() {
+		return cedula;
+	}
+	
+	String getNombre() {
+		return nombre;
+	}
+	
+	void setNombre(String nombre) { 
+		this.nombre = nombre; 
+	}
 
-    final long cedula;
-    String nombre;
-    static int totalPersonas;
-    
-    static {
-        totalPersonas = 0;
-        cedula = 3;
-    }
-
-    public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-    
-    public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-
-    public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
-    }
-
-    public Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
-    }
-    
-    public long getCedula() {
-        return cedula;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
